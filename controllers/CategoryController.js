24c3c9dc8f0112c47bb02aeb8cac198cae0eab5e
Zeyad Hegazy <zhegazy409@gmail.com>
2023-12-10 21:08:27 +0200

    get single category and add info in each handler

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -2,6 +2,9 @@ const slugify = require("slugify");
 const asyncHandler = require("express-async-handler");
 const Category = require("../models/CategoryModel");
 
+// @desc get list of categories
+// @route GET /api/v1/categories
+// @access Public
 const getCategories = asyncHandler(async (req, res) => {
 	const page = req.query.page * 1 || 1;
 	const limit = req.query.limit * 1 || 5;
@@ -12,6 +15,21 @@ const getCategories = asyncHandler(async (req, res) => {
 	res.status(200).json({ results: categories.length, page, data: categories });
 });
 
+// @desc get single category by it's id
+// @route GET /api/v1/categories/:id
+// @access Public
+const getCategory = asyncHandler(async (req, res) => {
+	const { id } = req.params;
+	const category = await Category.findById(id);
+	if (!category)
+		res.status(404).json({ message: "No Category found by this id" });
+
+	res.status(200).json({ data: category });
+});
+
+// @desc create category
+// @route POST /api/v1/categories
+// @access Private
 const createCategory = asyncHandler(async (req, res) => {
 	const { name } = req.body;
 	const category = await Category.create({ name, slug: slugify(name) });
@@ -21,4 +39,5 @@ const createCategory = asyncHandler(async (req, res) => {
 module.exports = {
 	getCategories,
 	createCategory,
+	getCategory,
 };
--- routes/CategoryRoute.js
+++ routes/CategoryRoute.js
@@ -5,8 +5,10 @@ const router = express.Router();
 const {
 	getCategories,
 	createCategory,
+	getCategory,
 } = require("../controllers/CategoryController");
 
 router.route("/").get(getCategories).post(createCategory);
+router.route("/:id").get(getCategory);
 
 module.exports = router;
