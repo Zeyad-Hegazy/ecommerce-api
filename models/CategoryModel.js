fb16b1cf7158c09cb2aefec78c328d5d1ac57e19
Zeyad Hegazy <zhegazy409@gmail.com>
2023-12-10 18:55:23 +0200

    Update CategoryModel.js

--- models/CategoryModel.js
+++ models/CategoryModel.js
@@ -1,8 +1,22 @@
 const mongoose = require("mongoose");
 
-const CategorySchema = new mongoose.Schema({
-	name: String,
-});
+const CategorySchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "Category name is required"],
+			unique: [true, "Category must be unique"],
+			minlength: [3, "Too short category name"],
+			maxlength: [32, "Too long category name"],
+		},
+
+		slug: {
+			type: String,
+			lowercase: true,
+		},
+	},
+	{ timestamps: true }
+);
 
 const Category = mongoose.model("Category", CategorySchema);
 
