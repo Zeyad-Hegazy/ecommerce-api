c68fca6872b42d1c3ab22ad3fa9375e5701199e9
Zeyad Hegazy <zhegazy409@gmail.com>
2023-12-15 10:47:35 +0200

    Update productValidator.js

--- util/validators/productValidator.js
+++ util/validators/productValidator.js
@@ -16,15 +16,75 @@ const createProductValidator = [
 			if (existingProduct) throw new Error("product title must be unique");
 		})
 		.isLength({ min: 2 })
-		.withMessage("Too short product name")
+		.withMessage("Too short product title")
 		.isLength({ max: 32 })
-		.withMessage("Too long product name"),
+		.withMessage("Too long product title"),
+	check("description")
+		.notEmpty()
+		.withMessage("Product description is required")
+		.isLength({ max: 200 })
+		.withMessage("Too long description"),
+	check("quantity")
+		.notEmpty()
+		.withMessage("product quantity is required")
+		.isNumeric()
+		.withMessage("price must be a number"),
+	check("sold")
+		.optional()
+		.isNumeric()
+		.withMessage("sold quantity must be number"),
+	check("price")
+		.notEmpty()
+		.withMessage("price is required")
+		.isNumeric()
+		.withMessage("price must be number")
+		.isLength({ max: 32 })
+		.withMessage("Too long price number"),
+	check("priceAfterDiscount")
+		.isNumeric()
+		.withMessage("price after discount must be a number")
+		.custom((value, { req }) => {
+			if (req.body.price <= value) {
+				throw new Error("price after discount must be lower than price");
+			}
+			return true;
+		}),
+	check("colors")
+		.optional()
+		.isArray()
+		.withMessage("colors must be an array of string"),
+	check("coverImage").notEmpty().withMessage("Cover image is required"),
+	check("images")
+		.optional()
+		.isArray()
+		.withMessage("images must be an array of string"),
+	check("category")
+		.notEmpty()
+		.withMessage("product must belong to category")
+		.isMongoId()
+		.withMessage("Invalid id format"),
+	check("subCategories")
+		.optional()
+		.isMongoId()
+		.withMessage("Invalid id format"),
+	check("brand").optional().isMongoId().withMessage("Invalid id format"),
+	check("ratingsAverage")
+		.optional()
+		.isNumeric()
+		.withMessage("ratings Average must be a number")
+		.isLength({ min: 1 })
+		.withMessage("Product Average must be above or equal 1.0")
+		.isLength({ max: 5 })
+		.withMessage("Product Average must be below or equal 5.0"),
+	check("ratingsQuantity")
+		.optional()
+		.isNumeric()
+		.withMessage("ratings quantity must be a number"),
 	validatorMiddleware,
 ];
 
 const updateProductValidator = [
 	check("id").isMongoId().withMessage("Invalid product id format"),
-	check("title").notEmpty(),
 	validatorMiddleware,
 ];
 
